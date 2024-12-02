427567f15c2bae5f059489de37481d31d3e93d54
navkaurneet <navkaurneet1@gmail.com>
2024-12-01 22:02:52 -0500

    Changing and updating the yml file of Azure Function CI/CD pipeline

--- tests/test-function.js
+++ tests/test-function.js
@@ -4,12 +4,12 @@ const httpFunction = require('../HttpTriggerFunction/index');
 describe('HTTP Trigger Function', () => {
     it('should return a personalized greeting when name is provided', async () => {
         const context = { log: () => {} };
-        const req = { query: { name: 'Edith' } };
+        const req = { query: { name: 'NAVNEET' } };
 
         await httpFunction(context, req);
 
         expect(context.res.status).to.equal(200);
-        expect(context.res.body).to.equal('Hello, Edith. This HTTP-triggered function executed successfully.');
+        expect(context.res.body).to.equal('Hello, NAVNEET. This HTTP-triggered function executed successfully.');
     });
 
     it('should return a default greeting when name is not provided', async () => {
@@ -19,6 +19,6 @@ describe('HTTP Trigger Function', () => {
         await httpFunction(context, req);
 
         expect(context.res.status).to.equal(200);
-        expect(context.res.body).to.equal('This HTTP-triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.');
+        expect(context.res.body).to.equal('This HTTP-triggered function executed successfully.');
     });
 });
